26a40448ec2de0998ffce6d5064508eb17ea77f2
andisltn48 <andiaril186@gmail.com>
2025-01-24 12:59:51 +0800

    feat: create api to submit blog

--- migrations/20250123154755_create_blog_posts_table.sql
+++ migrations/20250123154755_create_blog_posts_table.sql
@@ -1,5 +1,5 @@
 -- Add migration script here
-CREATE TABLE IF NOT EXISTS blog_spots (
+CREATE TABLE IF NOT EXISTS blog_posts (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use actix_web::{middleware::Logger, web, App, HttpServer, Responder, HttpResponse, get, post, put, delete};
+use core::fmt;
+
+use actix_web::{delete, get, middleware::Logger, post, put, web, App, HttpResponse, HttpServer, Responder, ResponseError};
 use env_logger;
 use serde::{Deserialize, Serialize};
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::{postgres::PgPoolOptions, prelude::FromRow, PgPool};
 
 
 //Create DB Fxn for connection
@@ -14,8 +16,8 @@ pub async fn establish_connection() -> Result<PgPool, sqlx::Error> {
 }
 
 //Models
-#[derive(Debug, Serialize, Deserialize)]
-struct BlogPost {
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+pub struct BlogPost {
     id: i32,
     title: String,
     content: String,
@@ -23,23 +25,108 @@ struct BlogPost {
 }
 
 //Input Serializer
-#[derive(Debug, Serialize, Deserialize)]
-struct NewBlogPost {
-    id: i32,
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+pub struct NewBlogPost {
     title: String,
     content: String,
     author: String
 }
 
+//SQLx functions
+pub async fn save_blog_post(pool: &PgPool, blog_post: &NewBlogPost) -> Result<BlogPost, sqlx::Error> {
+    let post = sqlx::query_as::<_, BlogPost>("INSERT INTO blog_posts (title, content, author) VALUES ($1, $2, $3) RETURNING *")
+    .bind(&blog_post.title)
+    .bind(&blog_post.content)
+    .bind(&blog_post.author)
+    .fetch_one(pool)
+    .await?;
+    Ok(post)
+}
+
+pub async fn find_all_post(pool: &PgPool) -> Result<Vec<BlogPost>, sqlx::Error> {
+    let posts = sqlx::query_as::<_, BlogPost>("SELECT * FROM blog_posts")
+    .fetch_all(pool)
+    .await?;
+    Ok(posts)
+}
+
+pub async fn find_post_by_id(pool: &PgPool, id: i32) -> Result<BlogPost, sqlx::Error> {
+    let post = sqlx::query_as::<_, BlogPost>("SELECT * FROM blog_posts WHERE id = $1")
+    .bind(id)
+    .fetch_one(pool)
+    .await?;
+    Ok(post)
+}
+
+pub async fn delete_post_by_id(pool: &PgPool, id: i32) -> Result<BlogPost, sqlx::Error> {
+    let post = sqlx::query_as::<_, BlogPost>("DELETE FROM blog_posts WHERE id = $1 RETURNING *")
+    .bind(id)
+    .fetch_one(pool)
+    .await?;
+    Ok(post)
+}
+
+pub async fn update_post_by_id(pool: &PgPool, id: i32, blog_post: &NewBlogPost) -> Result<BlogPost, sqlx::Error> {
+    let post = sqlx::query_as::<_, BlogPost>("UPDATE blog_posts SET title = $1, content = $2, author = $3 WHERE id = $4 RETURNING *")
+    .bind(&blog_post.title)
+    .bind(&blog_post.content)
+    .bind(&blog_post.author)
+    .bind(id)
+    .fetch_one(pool)
+    .await?;
+    Ok(post)
+}
+
+//Error handling
+#[derive(Debug, Serialize, Deserialize)]
+enum ApiError {
+    InternalError(String),
+    ValidationError(String),
+    NotFound(String),
+    DatabaseError(String)
+}
+
+impl ResponseError for ApiError {
+    fn error_response(&self) -> actix_web::HttpResponse {
+        match self {
+            ApiError::InternalError(msg) => actix_web::HttpResponse::InternalServerError().json(msg),
+            ApiError::ValidationError(msg) => actix_web::HttpResponse::BadRequest().json(msg),
+            ApiError::NotFound(msg) => actix_web::HttpResponse::NotFound().json(msg),
+            ApiError::DatabaseError(msg) => actix_web::HttpResponse::InternalServerError().json(msg)
+        }
+    }
+
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            ApiError::InternalError(_) => actix_web::http::StatusCode::INTERNAL_SERVER_ERROR,
+            ApiError::ValidationError(_) => actix_web::http::StatusCode::BAD_REQUEST,
+            ApiError::NotFound(_) => actix_web::http::StatusCode::NOT_FOUND,
+            ApiError::DatabaseError(_) => actix_web::http::StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+}
+
+impl fmt::Display for ApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ApiError::InternalError(msg) => write!(f, "Internal Server Error: {}", msg),
+            ApiError::ValidationError(msg) => write!(f, "Validation Error: {}", msg),
+            ApiError::NotFound(msg) => write!(f, "Not Found: {}", msg),
+            ApiError::DatabaseError(msg) => write!(f, "Database Error: {}", msg)
+        }
+    }
+}
+
 //Routes
-//CRUD routes
 async fn index_page() -> &'static str {
     "Hello world!"
 }
 
-// async fn create_blog_post() -> impl Responder {
-    
-// }
+#[post("/blog")]
+async fn create_blog_post(data: web::Data<PgPool>, new_post: web::Json<NewBlogPost>) -> Result<impl Responder, ApiError> {
+    let post = save_blog_post(&data, &new_post).await.expect("failed to save blog post");
+    Ok(HttpResponse::Ok().json(post))
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -58,6 +145,7 @@ async fn main() -> Result<(), std::io::Error> {
             .app_data(web::Data::new(pool.clone()))
             .wrap(Logger::default())
             .route("/", web::get().to(index_page))
+            .service(create_blog_post)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
